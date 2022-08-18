48f9e1c2719128b99a53878f75f1c707cc077489
jagaddhitaaryak <193040098.jagaddhita@mail.unpas.ac.id>
2022-08-18 13:34:17 +0700

    front end

---
+++ resources/views/landing-page.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Integral Technology - BPSDM Jabar</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="/css/mystyle.css">
+    <link rel="shortcut icon" href="/img/integral.png">
+</head>
+<body>
+    <!-- NAVBAR -->
+    <nav class="navbar navbar-expand-lg bg-primary navbar-dark sticky-top">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="img/logo-integral.png" alt="" width="90" height="55">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="#home">Beranda</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#about">Tentang</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#integral">Integral</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#contact">Kontak</a>
+                </li>
+                <li class="nav-item">
+                    <a class="btn btn-dark btn-login text-primary" href="/login">LOGIN</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <!-- TUTUP NAVBAR -->
+
+    <!-- HOME -->
+    <section id="home" class="bg-primary hero-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-7">
+                    <img src="/img/home.png" alt="" width="635" height="495" class="logo-home">
+                </div>
+                <div class="col-lg-5">
+                    <h2 class="head-title text-light">Integral Technology</h2>
+                    <p class="text-light fs-5">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et ex hic aperiam asperiores maiores dolore eveniet eligendi in reiciendis unde consequatur est officiis, ipsa odit qui cumque necessitatibus laboriosam iste.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="waves">
+         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0d6efd" fill-opacity="1" d="M0,160L80,144C160,128,320,96,480,101.3C640,107,800,149,960,170.7C1120,192,1280,192,1360,192L1440,192L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path></svg>
+    </div>
+
+    <!-- TUTUP HOME -->
+
+    <!-- ABOUT -->
+    <section id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <h2 class="head-tittle text-dark">Tentang Integral Technology</h2>
+                    <p class="fs-5 text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi at dolorum, laboriosam aliquid, perspiciatis in tempora quis nulla architecto eveniet doloribus corporis incidunt vitae? Soluta, animi provident officia pariatur sequi illo error accusantium hic magnam ipsa sit, id doloremque? Vero, omnis possimus tenetur laborum ratione dicta explicabo aspernatur consequuntur eaque.</p>
+                </div>
+                <div class="col-lg-5">
+                    <img src="/img/about.png" alt="" width="535" height="395" class="logo-about">
+                </div>
+            </div>
+        </div>
+    </section>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0d6efd" fill-opacity="1" d="M0,128L80,122.7C160,117,320,107,480,122.7C640,139,800,181,960,181.3C1120,181,1280,139,1360,117.3L1440,96L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
+    <!-- TUTUP ABOUT -->
+
+    <!-- INTEGRAL -->
+
+    
+
+    <section id="integral" class="bg-primary" height="600px">
+        <div class="container ">
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            <div class="col">
+                <div class="card h-100 text-center">
+                <img src="/img/eservice.png" class="card-img-top" alt="..." style="display:block; margin:auto;" width="100%" height="140">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100 text-center">
+                <img src="/img/multimedia.png" class="card-img-top" alt="..." style="display:block; margin:auto;" width="100%" height="140">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100 text-center">
+                <img src="/img/bigdata.png" class="card-img-top" alt="..." style="display:block; margin:auto;" width="100%" height="140">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        <!-- <div class="container">
+         <h2 class="head-tittle text-white text-center mb-5">Integral</h2>
+            <div class="row justify-content-center text-center">
+                <div class="col-lg-4 p-3">
+                    <div class="card p-4">
+                        <img src="/img/eservice.png" class="card-img-top" alt="..." style="display:block; margin:auto;">
+                        <div class="card-body">
+                            <p class="card-text">Berfungsi untuk melakukan proses Pelayanan Administrasi Penyelenggaraan Pelatihan mulai dari registrasi sampai dangan sertifikasi.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 p-3">
+                    <div class="card p-4">
+                        <img src="/img/multimedia.png" class="card-img-top" alt="..." style="display:block; margin:auto;">
+                        <div class="card-body">
+                            <p class="card-text">Berfungsi untuk pendokumentasian setiap kegiatan BPSDM,e-Sertifikat, pembuatan video bahan ajar Widyaiswara, serta pembuatan konten publikasi.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 p-3">
+                    <div class="card p-4">
+                        <img src="/img/bigdata.png" class="card-img-top" alt="..." style="display:block; margin:auto;">
+                        <div class="card-body">
+                            <p class="card-text">Berfungsi untuk menghimpun data keseluruhan aktifitas yang ada di BPSDM.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 p-3">
+                    <div class="card p-4">
+                        <img src="/img/aplikasi.png" class="card-img-top" alt="..." style="display:block; margin:auto;">
+                        <div class="card-body">
+                            <p class="card-text">Berfungsi untuk mengelola, memfasilitasi dan menerapkan aplikasi-aplikasi untuk mendukung proses learning secara mudah, lancar dan tersampaikan dengan baik. Memberikan laporan berkenaan dengan aplikasi yang ada di BPSDM Provinsi Jawa Barat.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 p-3">
+                    <div class="card p-4">
+                        <img src="/img/publikasi.png" class="card-img-top" alt="..." style="display:block; margin:auto;">
+                        <div class="card-body">
+                            <p class="card-text">Berfungsi untuk mengolah serta mempublikasikan informasi terkait kegiatan BPSDM Serta infomasi lainnya yang berhubungan dengan pengembangan Sumber Daya Manusia skala Nasional dan Internasional secara elektronik dan media cetak.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> -->
+    </section>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0d6efd" fill-opacity="1" d="M0,32L80,74.7C160,117,320,203,480,202.7C640,203,800,117,960,101.3C1120,85,1280,139,1360,165.3L1440,192L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path></svg> -->
+    <!-- TUTUP ITEGRAL
+
+    <!-- CONTACT -->
+    <section id="contact">
+        <div class="container text-center">
+            <h2 class="head-tittle pb-5">Kontak</h2>
+            <div class="row">
+                <div class="col-lg-4 ps-5">
+                    <i class="fa-solid fa-location-dot size-icon"></i>
+                    <p class="fs-5 fw-bold text-primary">Address</p>
+                    <p>
+                        Jl. Kolonel Masturi No.11, KM 3,5 <br>
+                        Cipageran, Cimahi Utara, <br>
+                        Kota Cimahi, Jawa Barat <br>
+                        40511
+                    </p>
+                </div>
+
+                <div class="col-lg-4">
+                    <i class="fa-solid fa-phone size-icon"></i>
+                    <br>
+                    <p class="fs-5 fw-bold text-primary">Phone</p>
+                    <p>+62 813-8283-0814</p>
+                </div>
+
+                <div class="col-lg-4">
+                    <i class="fa-solid fa-envelope size-icon"></i>
+                    <br>
+                    <p class="fs-5 fw-bold text-primary">Email</p>
+                    <p>simpanakuaja@gmail.com</p>
+                </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col-lg-6">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7922.727224400757!2d107.532326!3d-6.846941!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xf270b66e61664fe0!2sBpsdm%20Jabar!5e0!3m2!1sid!2sid!4v1660794248251!5m2!1sid!2sid" width="600" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <div class="col-lg-6">
+                    <img src="/img/location.svg" alt="" width="535" height="395" class="logo-about">
+            </div>
+        </div>
+    </section>
+    <!-- TUTUP CONTACT -->
+
+    <!-- FOOTER -->
+    <footer class="text-center bg-primary p-4 mt-5">
+        <span class="text-white">© 2022 Tim UNPAS.</span>
+    </footer>
+    <!-- AKHIR FOOTER -->
+
+
+
+
+
+
+
+    <!-- SCRIPT -->
+    <script src="https://kit.fontawesome.com/848026e0b0.js" crossorigin="anonymous"></script>
+    <!-- TUTUP SCRIPT -->
+</body>
+</html>
